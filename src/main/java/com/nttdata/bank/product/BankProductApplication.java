57f839ed8a5a562daab5dd17d23b8d0c2331931c
Gian Castro Morales <jesusgianmarco.castromorales@everis.nttdata.com>
2022-05-30 10:27:38 -0500

    commit

--- src/main/java/com/nttdata/bank/product/BankProductApplication.java
+++ src/main/java/com/nttdata/bank/product/BankProductApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.bank.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BankProductApplication {
 
 	public static void main(String[] args) {
